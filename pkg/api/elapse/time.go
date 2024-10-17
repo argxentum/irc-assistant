e79baec21a4cb4a36b67cbbf773bf9cce15eaf77
argentum <zargentum@proton.me>
2024-10-16 19:53:51 -0500

    fix time descriptions

--- pkg/api/elapse/time.go
+++ pkg/api/elapse/time.go
@@ -16,53 +16,39 @@ func TimeDescription(t time.Time) string {
 
 func PastTimeDescription(t time.Time) string {
 	elapsed := time.Now().Sub(t)
+	years, months, weeks, days, hours, minutes, seconds := units(elapsed)
 
-	year := time.Hour * 24 * 365
-	month := time.Hour * 24 * 30
-	week := time.Hour * 24 * 7
-	day := time.Hour * 24
-	hour := time.Hour
-	minute := time.Minute
-	second := time.Second
-
-	if elapsed >= year {
-		years := int(math.Round(float64(elapsed) / float64(year)))
+	if years > 0 {
 		if years == 1 {
 			return "last year"
 		}
 		return fmt.Sprintf("%d years ago", years)
-	} else if elapsed >= month {
-		months := int(math.Round(float64(elapsed) / float64(month)))
+	} else if months > 0 {
 		if months == 1 {
 			return "last month"
 		}
 		return fmt.Sprintf("%d months ago", months)
-	} else if elapsed >= week {
-		weeks := int(math.Round(float64(elapsed) / float64(week)))
+	} else if weeks > 0 {
 		if weeks == 1 {
 			return "last week"
 		}
 		return fmt.Sprintf("%d weeks ago", weeks)
-	} else if elapsed >= day {
-		days := int(math.Round(float64(elapsed) / float64(day)))
+	} else if days > 0 {
 		if days == 1 {
 			return "yesterday"
 		}
 		return fmt.Sprintf("%d days ago", days)
-	} else if elapsed >= hour {
-		hours := int(math.Round(float64(elapsed) / float64(day)))
+	} else if hours > 0 {
 		if hours == 1 {
 			return "an hour ago"
 		}
 		return fmt.Sprintf("%d hours ago", hours)
-	} else if elapsed >= minute {
-		minutes := int(math.Round(float64(elapsed) / float64(minute)))
+	} else if minutes > 0 {
 		if minutes == 1 {
 			return "a minute ago"
 		}
 		return fmt.Sprintf("%d minutes ago", minutes)
 	} else {
-		seconds := int(math.Round(float64(elapsed) / float64(second)))
 		if seconds < 5 {
 			return "just now"
 		}
@@ -75,7 +61,47 @@ func PastTimeDescription(t time.Time) string {
 
 func FutureTimeDescription(t time.Time) string {
 	elapsed := t.Sub(time.Now())
+	years, months, weeks, days, hours, minutes, seconds := units(elapsed)
 
+	if years > 0 {
+		if years == 1 {
+			return "next year"
+		}
+		return fmt.Sprintf("%d years from now", years)
+	} else if months > 0 {
+		if months == 1 {
+			return "next month"
+		}
+		return fmt.Sprintf("%d months from now", months)
+	} else if weeks > 0 {
+		if weeks == 1 {
+			return "next week"
+		}
+		return fmt.Sprintf("%d weeks from now", weeks)
+	} else if days > 0 {
+		if days == 1 {
+			return "tomorrow"
+		}
+		return fmt.Sprintf("%d days from now", days)
+	} else if hours > 0 {
+		if hours == 1 {
+			return "an hour from now"
+		}
+		return fmt.Sprintf("%d hours from now", hours)
+	} else if minutes > 0 {
+		if minutes == 1 {
+			return "a minute from now"
+		}
+		return fmt.Sprintf("%d minutes from now", minutes)
+	} else {
+		if seconds == 1 {
+			return "a second from now"
+		}
+		return fmt.Sprintf("%d seconds from now", seconds)
+	}
+}
+
+func units(d time.Duration) (years, months, weeks, days, hours, minutes, seconds int) {
 	year := time.Hour * 24 * 365
 	month := time.Hour * 24 * 30
 	week := time.Hour * 24 * 7
@@ -84,47 +110,37 @@ func FutureTimeDescription(t time.Time) string {
 	minute := time.Minute
 	second := time.Second
 
-	if elapsed >= year {
-		years := int(math.Round(float64(elapsed) / float64(year)))
-		if years == 1 {
-			return "next year"
-		}
-		return fmt.Sprintf("%d years from now", years)
-	} else if elapsed >= month {
-		months := int(math.Round(float64(elapsed) / float64(month)))
-		if months == 1 {
-			return "next month"
-		}
-		return fmt.Sprintf("%d months from now", months)
-	} else if elapsed >= week {
-		weeks := int(math.Round(float64(elapsed) / float64(week)))
-		if weeks == 1 {
-			return "next week"
-		}
-		return fmt.Sprintf("%d weeks from now", weeks)
-	} else if elapsed >= day {
-		days := int(math.Round(float64(elapsed) / float64(day)))
-		if days == 1 {
-			return "tomorrow"
-		}
-		return fmt.Sprintf("%d days from now", days)
-	} else if elapsed >= hour {
-		hours := int(math.Round(float64(elapsed) / float64(hour)))
-		if hours == 1 {
-			return "an hour from now"
-		}
-		return fmt.Sprintf("%d hours from now", hours)
-	} else if elapsed >= minute {
-		minutes := int(math.Round(float64(elapsed) / float64(minute)))
-		if minutes == 1 {
-			return "a minute from now"
-		}
-		return fmt.Sprintf("%d minutes from now", minutes)
-	} else {
-		seconds := int(math.Round(float64(elapsed) / float64(second)))
-		if seconds == 1 {
-			return "a second from now"
-		}
-		return fmt.Sprintf("%d seconds from now", seconds)
+	if d >= year {
+		years = measure(d, year)
+		d -= time.Duration(years) * year
 	}
+	if d >= month {
+		months = measure(d, month)
+		d -= time.Duration(months) * month
+	}
+	if d >= week {
+		weeks = measure(d, week)
+		d -= time.Duration(weeks) * week
+	}
+	if d >= day {
+		days = measure(d, day)
+		d -= time.Duration(days) * day
+	}
+	if d >= hour {
+		hours = measure(d, hour)
+		d -= time.Duration(hours) * hour
+	}
+	if d >= minute {
+		minutes = measure(d, minute)
+		d -= time.Duration(minutes) * minute
+	}
+	if d >= second {
+		seconds = measure(d, second)
+	}
+
+	return
+}
+
+func measure(a, b time.Duration) int {
+	return int(math.Round(float64(a) / float64(b)))
 }
