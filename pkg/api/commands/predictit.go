c000b5b85a156dbf78486619a6d302905756b83b
argentum <zargentum@proton.me>
2025-01-31 13:20:52 -0600

    show more market contracts

--- pkg/api/commands/predictit.go
+++ pkg/api/commands/predictit.go
@@ -17,6 +17,7 @@ import (
 const PredictItCommandName = "predictIt"
 
 const predictItSearchBaseURL = "https://www.predictit.org/api/Browse/Search"
+const predictItMarketURL = "https://www.predictit.org/api/marketdata/markets/%d"
 const predictItMarketDetailURL = "https://www.predictit.org/markets/detail/%d"
 
 type PredictItCommand struct {
@@ -63,8 +64,21 @@ type predictItSearchResult struct {
 			Name   string  `json:"contractName"`
 			Price  float64 `json:"lastTradePrice"`
 			Trades int     `json:"totalTrades"`
-		}
+		} `json:"contracts"`
 	}
+}
+
+type predictItMarketResult struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	ShortName string `json:"shortName"`
+	URL       string `json:"url"`
+	Contracts []struct {
+		ID     int     `json:"id"`
+		Name   string  `json:"name"`
+		Price  float64 `json:"lastTradePrice"`
+		Trades int     `json:"totalTrades"`
+	} `json:"contracts"`
 }
 
 func (c *PredictItCommand) Execute(e *irc.Event) {
@@ -119,47 +133,75 @@ func (c *PredictItCommand) Execute(e *irc.Event) {
 		return
 	}
 
-	defer resp.Body.Close()
-
-	var result predictItSearchResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	var searchResult predictItSearchResult
+	if err = json.NewDecoder(resp.Body).Decode(&searchResult); err != nil {
 		logger.Errorf(e, "error decoding predictIt response: %s", err)
 		c.Replyf(e, "unable to process PredictIt result for %s", style.Bold(input))
 		return
 	}
 
-	if len(result.Markets) == 0 {
+	resp.Body.Close()
+
+	if len(searchResult.Markets) == 0 {
 		logger.Debugf(e, "no markets found for %s", input)
 		c.Replyf(e, "no PredictIt markets found for %s", style.Bold(input))
 		return
 	}
 
-	market := result.Markets[0]
+	searchResultMarket := searchResult.Markets[0]
+
+	if len(searchResultMarket.Contracts) == 0 {
+		logger.Debugf(e, "no contracts found for %s", searchResultMarket.Name)
+		c.Replyf(e, "no PredictIt contracts found for %s", style.Bold(searchResultMarket.Name))
+		return
+	}
+
+	resp, err = http.Get(fmt.Sprintf(predictItMarketURL, searchResultMarket.ID))
+	if err != nil {
+		logger.Errorf(e, "error retrieving predictIt market: %s", err)
+		c.Replyf(e, "unable to retrieve PredictIt market for %s", style.Bold(searchResultMarket.Name))
+		return
+	}
+
+	var marketResult predictItMarketResult
+	if err = json.NewDecoder(resp.Body).Decode(&marketResult); err != nil {
+		logger.Errorf(e, "error decoding predictIt market: %s", err)
+		c.Replyf(e, "unable to process PredictIt market for %s", style.Bold(searchResultMarket.Name))
+		return
+	}
+
+	resp.Body.Close()
 
 	contracts := make([]string, 0)
 	maxYes := 0.0
-	for _, contract := range market.Contracts {
+	for _, contract := range marketResult.Contracts {
 		if contract.Price > maxYes {
 			maxYes = contract.Price
 		}
 	}
 
-	for _, contract := range market.Contracts {
+	for _, contract := range marketResult.Contracts {
 		message := fmt.Sprintf("%s: $%.02f", style.Underline(contract.Name), contract.Price)
+
+		trades := ""
+		if contract.Trades > 0 {
+			trades = fmt.Sprintf("(%s trades)", text.DecorateNumberWithCommas(contract.Trades))
+		}
+
 		if contract.Price == maxYes {
-			contracts = append(contracts, fmt.Sprintf("%s (%s trades)", style.ColorForeground(message, style.ColorGreen), text.DecorateNumberWithCommas(contract.Trades)))
+			contracts = append(contracts, fmt.Sprintf("%s%s", style.ColorForeground(message, style.ColorGreen), trades))
 		} else {
-			contracts = append(contracts, fmt.Sprintf("%s (%s trades)", message, text.DecorateNumberWithCommas(contract.Trades)))
+			contracts = append(contracts, fmt.Sprintf("%s%s", message, trades))
 		}
 	}
 
 	detail := ""
-	if market.Status != "Open" {
-		detail = fmt.Sprintf(" (%s)", style.ColorForeground(market.Status, style.ColorRed))
+	if searchResultMarket.Status != "Open" {
+		detail = fmt.Sprintf(" (%s)", style.ColorForeground(searchResultMarket.Status, style.ColorRed))
 	}
 
 	c.SendMessages(e, e.ReplyTarget(), []string{
-		fmt.Sprintf("%s%s • %s", style.Bold(market.Name), detail, strings.Join(contracts, " | ")),
-		fmt.Sprintf(predictItMarketDetailURL, market.ID),
+		fmt.Sprintf("%s%s • %s", style.Bold(searchResultMarket.Name), detail, strings.Join(contracts, " | ")),
+		fmt.Sprintf(predictItMarketDetailURL, searchResultMarket.ID),
 	})
 }
