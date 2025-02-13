8cba2acd9883df64d0553d30c4132a0b6734c3cc
argentum <zargentum@proton.me>
2025-02-13 15:14:21 -0600

    add additional impersonation headers

--- pkg/api/retriever/retriever.go
+++ pkg/api/retriever/retriever.go
@@ -58,6 +58,18 @@ var headerSets = []map[string]string{
 		"Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 		"Accept-Language": "en-US,en;q=0.9",
 	},
+	{
+		"User-Agent":         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+		"Accept":             "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"Accept-Language":    "en-US,en;q=0.9,eo;q=0.8",
+		"sec-ch-ua":          `"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"`,
+		"sec-ch-ua-mobile":   "?0",
+		"sec-ch-ua-platform": `"Linux"`,
+		"sec-fetch-dest":     "document",
+		"sec-fetch-mode":     "navigate",
+		"sec-fetch-site":     "none",
+		"sec-fetch-user":     "?1",
+	},
 }
 
 var allowedContentTypePrefixes = []string{
