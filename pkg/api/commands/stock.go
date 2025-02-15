fb7db706326b7c5d4a578c2900af38ba792868b7
argentum <zargentum@proton.me>
2025-02-14 20:42:00 -0600

    improved stock parsing

--- pkg/api/commands/stock.go
+++ pkg/api/commands/stock.go
@@ -71,18 +71,31 @@ func (c *StockCommand) retrieveStockPriceMessage(e *irc.Event, symbol string) st
 	logger := log.Logger()
 
 	query := url.QueryEscape(fmt.Sprintf("current stock price %s", strings.ToUpper(symbol)))
-	section, err := c.retriever.RetrieveDocumentSelection(e, retriever.DefaultParams(fmt.Sprintf(bingSearchURL, query)), "div.finquote")
+	section, err := c.retriever.RetrieveDocumentSelection(e, retriever.DefaultParams(fmt.Sprintf(bingSearchURL, query)), "html")
 	if err != nil {
 		logger.Warningf(e, "unable to retrieve stock price data for %s: %s", symbol, err)
 		return ""
 	}
 
-	title := strings.TrimSpace(section.Find("h2").First().Text())
-	subtitle := strings.TrimSpace(section.Find("div.fin_metadata").First().Text())
-	priceSection := section.Find("div.fin_quotePrice").First()
+	title := strings.TrimSpace(section.Find("div.enti_ttl").Text())
+	if len(title) == 0 {
+		title = strings.TrimSpace(section.Find("h2").First().Text())
+	}
+
+	subtitle := strings.TrimSpace(section.Find("div.enti_stxt").Text())
+	if len(subtitle) == 0 {
+		subtitle = strings.TrimSpace(section.Find("div.fin_metadata").First().Text())
+	}
+
+	priceSection := section.Find("div.fin_quoteCard").First()
 	price := strings.TrimSpace(priceSection.Find("div#Finance_Quote").First().Text())
 	currency := strings.TrimSpace(priceSection.Find("span.price_curr").First().Text())
 	change := strings.TrimSpace(priceSection.Find("span.fin_change").First().Text())
+	open := strings.TrimSpace(priceSection.Find("div[data-partnertag='Finance.Open']").Next().Text())
+	high := strings.TrimSpace(priceSection.Find("div[data-partnertag='Finance.High']").Next().Text())
+	low := strings.TrimSpace(priceSection.Find("div[data-partnertag='Finance.Low']").Next().Text())
+	yearHigh := strings.TrimSpace(priceSection.Find("div[data-partnertag='Finance.YeahHigh']").Next().Text())
+	yearLow := strings.TrimSpace(priceSection.Find("div[data-partnertag='Finance.YeahLow']").Next().Text())
 
 	if len(title) == 0 || len(price) == 0 {
 		logger.Warningf(e, "unable to parse stock market information, title: %s, price: %s", title, price)
@@ -102,5 +115,26 @@ func (c *StockCommand) retrieveStockPriceMessage(e *irc.Event, symbol string) st
 	}
 
 	message += fmt.Sprintf("%s %s %s", price, currency, styledChange)
+
+	if len(open) > 0 {
+		message += fmt.Sprintf(" | Open: %s", open)
+	}
+
+	if len(high) > 0 {
+		message += fmt.Sprintf(" | High: %s", high)
+	}
+
+	if len(low) > 0 {
+		message += fmt.Sprintf(" | Low: %s", low)
+	}
+
+	if len(yearHigh) > 0 {
+		message += fmt.Sprintf(" | 52W High: %s", yearHigh)
+	}
+
+	if len(yearLow) > 0 {
+		message += fmt.Sprintf(" | 52W Low: %s", yearLow)
+	}
+
 	return message
 }
